e155aaaba04093f3c896e29770a1c51ce8b51c12
Juan Tenezaca <tenj34@gmail.com>
2017-10-23 21:17:42 -0400

    Day 2
    Added a table view and added a second story board

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet weak var thetableView: UITableView!
+    
+    
+    var emojis = ["👺","👿","🤖","👻","👾","🍆"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // this is where it asks questions on how many things will need to go into the tableview
+        thetableView.dataSource = self
+        thetableView.delegate = self
+        
     }
 
+    //  How many rows do you want
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    //  What you want inside of your row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
